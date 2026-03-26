a71a42bfa254fe03811f21a5c0902d756db04d00
yun <coolpyh77@gmail.com>
2026-03-26 19:35:44 +0900

    spring 실습 제출 2주차

---
+++ yunhyeok/week2/practice/practice_0326/src/main/java/mustsa/practice_0326/Practice0326Application.java
@@ -0,0 +1,13 @@
+package mustsa.practice_0326;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practice0326Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practice0326Application.class, args);
+	}
+
+}
---
+++ yunhyeok/week2/practice/practice_0326/src/test/java/mustsa/practice_0326/Practice0326ApplicationTests.java
@@ -0,0 +1,13 @@
+package mustsa.practice_0326;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practice0326ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
