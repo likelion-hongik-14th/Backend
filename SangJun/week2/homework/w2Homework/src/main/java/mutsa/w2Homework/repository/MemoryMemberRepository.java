2bc01fcc161dd7a780d2b2d4d4aec29ed15e96db
KangSangJun <befur1@g.hongik.ac.kr>
2026-04-01 23:29:29 +0900

    [Feat] w2Hw 스프링 빈과 의존관계

--- SangJun/week2/homework/w2Homework/src/main/java/mutsa/w2Homework/repository/MemoryMemberRepository.java
+++ SangJun/week2/homework/w2Homework/src/main/java/mutsa/w2Homework/repository/MemoryMemberRepository.java
@@ -1,8 +1,10 @@
 package mutsa.w2Homework.repository;
 
 import mutsa.w2Homework.domain.Member;
+import org.springframework.stereotype.Repository;
 
 import java.util.*;
+
 
 public class MemoryMemberRepository implements MemberRepository{
 
--- SangJun/week2/homework/w2Homework/src/main/java/mutsa/w2Homework/service/MemberService.java
+++ SangJun/week2/homework/w2Homework/src/main/java/mutsa/w2Homework/service/MemberService.java
@@ -3,13 +3,17 @@ package mutsa.w2Homework.service;
 import mutsa.w2Homework.domain.Member;
 import mutsa.w2Homework.repository.MemberRepository;
 import mutsa.w2Homework.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+
 public class MemberService {
 
     private final MemberRepository memberRepository;
+
 
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
