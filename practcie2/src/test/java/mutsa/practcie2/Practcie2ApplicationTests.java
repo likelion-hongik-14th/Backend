9be22ff28e2f601aaf99c7239560cc2bc2a687b1
yun <coolpyh77@gmail.com>
2026-04-01 11:57:56 +0900

    [Fix] ignore 해ê

---
+++ practcie2/src/test/java/mutsa/practcie2/Practcie2ApplicationTests.java
@@ -0,0 +1,13 @@
+package mutsa.practcie2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practcie2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
