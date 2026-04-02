c6fcf5d7249b6b18663309d5a9355715fd16d645
leehanjay <128954406+leehanjay@users.noreply.github.com>
2026-04-02 19:21:14 +0900

    [Feat]week3 실습 제출

---
+++ hanjay/week2/practice/src/main/java/mutsa/session/ArticleController.java
@@ -0,0 +1,28 @@
+package mutsa.session;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/articles")
+@RequiredArgsConstructor
+public class ArticleController {
+
+    private final ArticleService articleService;
+
+    //1.기사 작성(POST)
+    @PostMapping
+    public ResponseEntity<ArticleResponseDto> createArticle(@RequestBody ArticleCreateRequestDto requestDto) {
+        ArticleResponseDto responseDto = articleService.createArticle(requestDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseDto);
+    }
+
+    //2.기사 단건 조회
+    @GetMapping("/{articleid}")
+    public ResponseEntity<ArticleResponseDto> getArticle(@PathVariable Long articleid) {
+        ArticleResponseDto responseDto = articleService.getArticle(articleid);
+        return ResponseEntity.ok(responseDto);
+    }
+}
