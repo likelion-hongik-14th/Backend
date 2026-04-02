fe32311c0a07c2e13c8264df286b02e939ade547
uaeoon <hhaaee9153@g.hongik.ac.kr>
2026-04-02 19:10:37 +0900

    [Feat] 3주차 실습 완료

---
+++ haeyoon/week3/practice/src/main/java/mutsa/session2/ArticleResponseDto.java
@@ -0,0 +1,16 @@
+package mutsa.session2;
+
+import lombok.Getter;
+
+@Getter
+public class ArticleResponseDto {
+    private Long id;
+    private String title;
+    private String content;
+
+    public ArticleResponseDto(Article article) {
+        this.id = article.getId();
+        this.title = article.getTitle();
+        this.content = article.getContent();
+    }
+}
