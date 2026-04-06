60b682912d9258bae5b76104926ab31569e954bb
leehanjay <128954406+leehanjay@users.noreply.github.com>
2026-04-07 02:27:56 +0900

    [Feat]week3 과제 제출

--- hanjay/week3/homework/src/main/java/hello/hello_spring/service/MemberService.java
+++ hanjay/week3/homework/src/main/java/hello/hello_spring/service/MemberService.java
@@ -3,13 +3,14 @@ package hello.hello_spring.service;
 import hello.hello_spring.domain.Member;
 import hello.hello_spring.repository.MemberRepository;
 import hello.hello_spring.repository.MemoryMemberRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
-
+@Transactional
 public class MemberService {
 
     private final MemberRepository memberRepository;
@@ -21,9 +22,18 @@ public class MemberService {
      * 회원가입
      */
     public Long join(Member member) {
-        validateDuplicateMember(member); //중복 회원 검증
-        memberRepository.save(member);
-        return member.getId();
+
+        long start = System.currentTimeMillis();
+
+        try {
+            validateDuplicateMember(member); //중복 회원 검증
+            memberRepository.save(member);
+            return member.getId();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("join " + timeMs + "ms");
+        }
     }
     private void validateDuplicateMember(Member member) {
         memberRepository.findByName(member.getName())
@@ -35,8 +45,17 @@ public class MemberService {
      * 전체 회원 조회
      */
     public List<Member> findMembers() {
-        return memberRepository.findAll();
+
+        long start = System.currentTimeMillis();
+        try {
+            return memberRepository.findAll();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("findMembers " + timeMs + "ms");
+        }
     }
+
     public Optional<Member> findOne(Long memberId) {
         return memberRepository.findById(memberId);
     }
