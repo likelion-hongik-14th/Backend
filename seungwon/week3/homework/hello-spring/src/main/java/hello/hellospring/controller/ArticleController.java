a07e410eafc806f5612b924b75338a442fa33039
Seungwon32 <bsw030722@gmail.com>
2026-04-08 01:13:42 +0900

    [Feat] week3 homework 과제 제출

---
+++ seungwon/week3/homework/hello-spring/src/main/java/hello/hellospring/controller/ArticleController.java
@@ -0,0 +1,29 @@
+package hello.hellospring.controller;
+
+import hello.hellospring.Entitiy.Article;
+import hello.hellospring.dto.ArticleCreateRequestDto;
+import hello.hellospring.dto.ArticleResponseDto;
+import hello.hellospring.service.ArticleService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/articles")
+@RequiredArgsConstructor
+public class ArticleController {
+    private final ArticleService articleService;
+
+    @PostMapping
+    public ResponseEntity<ArticleResponseDto> createArticle(@RequestBody ArticleCreateRequestDto requestDto){
+        ArticleResponseDto responseDto = articleService.createArticle(requestDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseDto);
+    }
+
+    @GetMapping("/{articleId}")
+    public ResponseEntity<ArticleResponseDto> getArticle(@PathVariable Long articleId){
+        ArticleResponseDto responseDto = articleService.getArticle(articleId);
+        return ResponseEntity.ok(responseDto);
+    }
+}
