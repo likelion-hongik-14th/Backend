4b38342bbd358f2da8760c5f85fcdc766942843b
Seunghwan Song <bourne11594@gmail.com>
2026-04-08 18:50:06 +0900

    [Feat] Week2 API 실ìk3 실습 d완 과제 제출

---
+++ seunghwan/week3/homework/src/main/java/hello/hello_spring/SpringConfig.java
@@ -0,0 +1,73 @@
+package hello.hello_spring;
+
+/*
+import hello.hello_spring.repository.JdbcMemberRepository;
+import hello.hello_spring.repository.JdbcTemplateMemberRepository;
+import hello.hello_spring.repository.MemberRepository;
+import hello.hello_spring.repository.MemoryMemberRepository;
+import hello.hello_spring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class SpringConfig {
+
+    private final DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        //return new MemoryMemberRepository();
+        //return new JdbcMemberRepository(dataSource);
+        return new JdbcTemplateMemberRepository(dataSource);
+    }
+}
+*/
+
+import hello.hello_spring.aop.TimeTraceAop;
+import hello.hello_spring.repository.*;
+import hello.hello_spring.service.MemberService;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class SpringConfig {
+
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Bean
+    public MemberService memberService() {
+
+        return new MemberService(memberRepository);
+    }
+
+/*
+    @Bean
+    public MemberRepository memberRepository() {
+
+        //return new JpaMemberRepository(em);
+    }
+*/
+}
