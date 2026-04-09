dfbc0f1145764666555b6dfb8f5dff9bc8b03041
kimhojun0428 <jade5889>
2026-04-09 19:50:32 +0900

    4주차 실습

--- hojun/week4/practice/session/src/main/java/practice/session/Article.java
+++ hojun/week4/practice/session/src/main/java/practice/session/Article.java
@@ -1,12 +1,20 @@
 package practice.session;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @Getter
-@Setter
+@Entity
+@RequiredArgsConstructor
 public class Article {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String content;
--- hojun/week4/practice/session/src/main/java/practice/session/ArticleResponseDto.java
+++ hojun/week4/practice/session/src/main/java/practice/session/ArticleResponseDto.java
@@ -1,9 +1,19 @@
 package practice.session;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
+@Entity
+@RequiredArgsConstructor
 public class ArticleResponseDto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String content;
