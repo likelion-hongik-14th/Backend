2e5d60d2e20ff34065ddc25ca61ab89bb4e3aed1
KangSangJun <befur1@g.hongik.ac.kr>
2026-05-26 14:45:09 +0900

    [Fix] 주소 삭제 로직 예외처리 및 불필요한 응답 삭제

--- SangJun/week7/practice/w5Homework/src/main/java/mutsa/w5homework/controller/AddressController.java
+++ SangJun/week7/practice/w5Homework/src/main/java/mutsa/w5homework/controller/AddressController.java
@@ -1,9 +1,14 @@
 package mutsa.w5homework.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import mutsa.w5homework.domain.Address;
 import mutsa.w5homework.dto.AddressDto;
+import mutsa.w5homework.global.apiPayload.ApiResponse;
+import mutsa.w5homework.repository.AddressRepository;
 import mutsa.w5homework.service.AddressService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,9 +18,10 @@ import java.util.List;
 @RequestMapping("/v1/addresses")
 public class AddressController {
     private final AddressService addressService;
+    private final AddressRepository addressRepository;
 
     @PostMapping
-    public ResponseEntity<AddressDto.Response> createAddress(@RequestBody AddressDto.CreateRequest dto) {
+    public ResponseEntity<AddressDto.Response> createAddress(@Valid @RequestBody AddressDto.CreateRequest dto) {
         AddressDto.Response response = addressService.createAddress(dto);
         return ResponseEntity.ok(response);
     }
@@ -29,14 +35,17 @@ public class AddressController {
 
     //배송지 수정
     @PutMapping("/{addressId}")
-    public ResponseEntity<AddressDto.Response> updateAddress(@PathVariable Long addressId, @RequestBody AddressDto.UpdateRequest dto) {
+    public ResponseEntity<AddressDto.Response> updateAddress(@Valid @PathVariable Long addressId, @RequestBody AddressDto.UpdateRequest dto) {
         AddressDto.Response response = addressService.updateAddress(addressId, dto);
         return ResponseEntity.ok(response);
     }
 
     @DeleteMapping("/{addressId}")
-    public ResponseEntity<AddressDto.Response> deleteAddress(@PathVariable Long addressId) {
+    public ApiResponse<Void> deleteAddress(@PathVariable Long addressId) {
+        //유효한 주소Id인지 검증
+        Address address = addressRepository.findById(addressId).orElseThrow(()
+                -> new RuntimeException("Address not found"));
         AddressDto.Response response = addressService.deleteAddress(addressId);
-        return ResponseEntity.ok(response);
+        return ApiResponse.onSuccess("주소록 삭제 성공", null);
     }
 }
